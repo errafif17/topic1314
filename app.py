ca098611779d4f9433f343ea22d7387be8f51c4e
Ertansa Rafif Rianto <77961216+errafif17@users.noreply.github.com>
2022-11-25 11:30:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ ST_Slope = st.selectbox('The slope of the peak exercise ST segment',df['ST_Slope
 if st.button('Predict Heart Health'):
     query = np.array([Age,Sex,ChestPainType,RestingBP,Cholesterol,FastingBS,RestingECG,MaxHR,ExerciseAngina,Oldpeak,ST_Slope])
     query = query.reshape(1, 11)
-    result=str(lr.predict(query)[0])
+    result=str(pipe.predict(query)[0])
     if result == 1:
         st.header("You've a heart disease. Please, consult your doctor")
     else:
