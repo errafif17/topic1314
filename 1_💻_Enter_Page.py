debe22f613d31f2d2164abcfc5cbe49ea459ac6f
Ertansa Rafif Rianto <77961216+errafif17@users.noreply.github.com>
2022-11-25 13:14:30 +0700

    Update 1_💻_Enter_Page.py

--- 1_💻_Enter_Page.py
+++ 1_💻_Enter_Page.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from streamlit.logger import get_logger
 import streamlit.components.v1 as components
-import datetime
 
 LOGGER = get_logger(__name__)
-
-thedate = datetime.date.today()  
 
 if __name__ == "__main__":
     run()
